f2a6a265661bf71ad4b6371a867dc488842f9103
mortyrex34 <achraferrabbaa1@gmail.com>
2024-07-26 16:16:28 +0100

    commit tanya

--- public/js/app.js
+++ public/js/app.js
@@ -32,6 +32,7 @@ while (true) {
     break;
 }else if (choice === "sign up") {
     let fullName = prompt("enter your full name:").trim()
+    fullName = fullName.charAt(0).toUpperCase() + fullName.slice(1).toLowerCase()
     let email = prompt("enter your email:")
     let age = prompt("enter your age:")
     let password = prompt("enter your password:")
