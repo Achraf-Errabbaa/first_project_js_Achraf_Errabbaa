4bb7321cd7db13dae2d19fb214f85e31a9503ed2
mortyrex34 <achraferrabbaa1@gmail.com>
2024-07-27 00:47:27 +0100

    commit lili1

--- public/js/app.js
+++ public/js/app.js
@@ -1,57 +1,78 @@
 class bank {
-    constructor(){
+    constructor() {
         this.user = {};
     }
-    signUp(fullName, email, age, password){
-        this.user[email] = {fullName,age,email , password}
+    signUp(fullName, email, age, password) {
+        this.user[email] = { fullName, age, email, password }
         console.log("your acount has been created");
     }
-    logIn(email,password){
-    if (this.user[email] && this.user[email].password == password ) {
-        console.log("you are logged in");
-    }else{
-        console.log("invalid email or password");
+    logIn(email, password) {
+        if (this.user[email] && this.user[email].password == password) {
+            console.log("you are logged in");
+        } else {
+            console.log("invalid email or password");
+        }
     }
+    changePassword(email, currentPassword, newPassword) {
+        if (this.user[email] && this.user[email].password == currentPassword) {
+            this.user[email].password = newPassword;
+            console.log("password changed successfully");
+        } else {
+            console.log("invalid email or password");
+        }
     }
-    changePassword(email, currentPassword, newPassword){
-    if (this.user[email] && this.user[email].password == currentPassword) {
-        this.user[email].password = newPassword;
-        console.log("password changed successfully");
-    }else{
-        console.log("invalid email or password");
+    validateName(fullName) {
+        return fullName.length >= 5;
     }
+    validateSpecial(fullName){
+        return!/[&\/\\#,+()$~%.'":*?<>{}]/.test(fullName);
     }
+    validateEmail(email){
+    email = email.replace(/\s/g, '');
+    }
+
 }
 
 let account = new bank();
 while (true) {
     let choice = prompt("choose an option:sign up || log in || change Password || exit ");
     choice = choice.toLowerCase()
-    if(choice === "exit") {
-    console.log("bye");
-    break;
-}else if (choice === "sign up") {
-    let fullName = prompt("enter your full name:").trim()
-    fullName = fullName.split(" ").map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(" ");
-    let email = prompt("enter your email:")
-    let age = prompt("enter your age:")
-    let password = prompt("enter your password:")
-    account.signUp(fullName, email, age, password);
-    }else if (choice === "log in") {
+    if (choice === "exit") {
+        console.log("bye");
+        break;
+    } else if (choice === "sign up") {
+        let fullName = prompt("enter your full name:").trim()
+        fullName = fullName.split(" ").map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(" ");
+        let email = prompt("enter your email:").trim()
+        if (!account.validateEmail(email)) {
+            console.log("invalid email");
+            
+        }
+        let age = prompt("enter your age:")
+        let password = prompt("enter your password:")
+        if (!account.validateName(fullName)) {
+            console.log("invalid name");
+            continue
+        }
+        if (!account.validateSpecial(fullName)) {
+            console.log("special character or number detected");
+            continue
+        }
+        account.signUp(fullName, email, age, password);
+    } else if (choice === "log in") {
         let email = prompt("enter your email:")
         let password = prompt("enter your password:")
+        
         account.logIn(email, password)
-    }else if (choice === "change password") {
+    } else if (choice === "change password") {
         let email = prompt("enter your email:")
         let currentPassword = prompt("enter your current password:")
         let newPassword = prompt("enter your new password:")
-        account.changePassword(email, password , currentPassword, newPassword);
+        account.changePassword(email, password, currentPassword, newPassword);
         break;
-    }else{
+    } else {
         console.log("your choice was invalid try again");
     }
 }
-
-
 console.log(account.user);
 
